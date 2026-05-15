4cc4de12578acdc0539a87eda7aca51ac099075b
Krongkarn13 <98693530+Krongkarn13@users.noreply.github.com>
2026-05-15 21:39:12 +0700

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -8,7 +8,10 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId:   process.env.FIREBASE_PROJECT_ID,
       privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-      privateKey:  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+  ?.replace(/\\n/g, '\n')
+  ?.replace(/^"|"$/g, '')   // ตัด " ที่หัวท้ายออกถ้ามี
+  ?.trim(),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       clientId:    process.env.FIREBASE_CLIENT_ID,
     }),
